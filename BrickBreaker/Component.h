4ae31014fb6b4bf4a5a3f5c660646a2f1b1a6c34
J-Contois <jonathan.contois@gmail.com>
2024-11-08 18:00:02 +0100

    feat(Component): add component

---
+++ BrickBreaker/Component.h
@@ -0,0 +1,30 @@
+#pragma once
+struct position
+{
+	float posX;
+	float posY;
+};
+
+struct velocity
+{
+	float veloX;
+	float veloY;
+};
+
+struct size
+{
+	float width;
+	float height;
+};
+
+struct color
+{
+	float red;
+	float green;
+	float blue;
+};
+
+struct texture
+{
+	char* texture;
+};
